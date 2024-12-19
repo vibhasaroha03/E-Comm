9519d1e6435e2fc9bd1a118c181792f0dd04dc6b
Vibha Saroha <122997401+vibhasaroha03@users.noreply.github.com>
2024-12-19 13:01:39 +0000

    added user model part-1

--- modals/userschema.js
+++ modals/userschema.js
@@ -1,12 +1,48 @@
 import mongoose from "mongoose";
-
-const userSchema = mongoose.Schema({
+import AuthRoles from "../utilis/authRoles"
+const userSchema = mongoose.Schema(
+    {
 
     name:{
         type:String,
         required:[true,"Name is required"],
         maxLength:[50,"Name must be less than 50"]
-    }
-})
+    },
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+       unique:true   
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+       minLength:[8,"password must be atleast 8 character long"] ,
+    select:false
+
+    },
+role:{
+
+    type:String,
+
+enum:Object.values(AuthRoles),
+default:AuthRoles.USER 
+
+
+},
+forgotPasswordToken:String,
+forgotPasswordExpiry:Date,
+
+
+},
+{
+
+timestamps:true
+
+
+}
+
+
+);
+export default mongoose.model("User",userSchema)
 
 
