dad8c442de9b3f5ff2b2da8678488eaeaf302404
Vibha Saroha <122997401+vibhasaroha03@users.noreply.github.com>
2024-12-19 13:20:43 +0000

    hooks for userSchema

--- modals/userschema.js
+++ modals/userschema.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utilis/authRoles"
+import bcrypt from "bcryptjs"
+import JWT from "jsonwebtoken"
+import crypto from "crypto"
+
+
 const userSchema = mongoose.Schema(
     {
-
     name:{
         type:String,
         required:[true,"Name is required"],
@@ -18,31 +22,30 @@ const userSchema = mongoose.Schema(
         required:[true,"Password is required"],
        minLength:[8,"password must be atleast 8 character long"] ,
     select:false
-
-    },
+  },
 role:{
-
     type:String,
-
 enum:Object.values(AuthRoles),
 default:AuthRoles.USER 
-
-
 },
 forgotPasswordToken:String,
 forgotPasswordExpiry:Date,
-
-
 },
 {
-
 timestamps:true
-
-
 }
-
-
 );
+
+//encrypt password
+//pre is hook in mongoose
+userSchema.pre("save",async function(next){
+    if(!this.modified("password"))return next();
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+
+
+
 export default mongoose.model("User",userSchema)
 
 
